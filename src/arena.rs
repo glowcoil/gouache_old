c95b3a8cd56c69d5600b99e6e764169867484ee9
Micah Johnston <micah@glowcoil.com>
2019-02-27 19:43:16 -0600

    slice allocation in arena

--- src/arena.rs
+++ src/arena.rs
@@ -2,20 +2,16 @@ use std::mem;
 
 pub struct Arena {
     data: Vec<Vec<u8>>,
-    next_size: usize,
 }
 
 impl Arena {
     pub fn with_capacity(capacity: usize) -> Arena {
         Arena {
             data: vec![Vec::with_capacity(capacity)],
-            next_size: capacity,
         }
     }
 
-    pub fn alloc<'a, T: Copy>(&mut self, value: T) -> &'a mut T where Self: 'a {
-        let bytes = mem::size_of::<T>();
-        let align = mem::align_of::<T>();
+    fn alloc_bytes(&mut self, bytes: usize, align: usize) -> *mut u8 {
         let (ptr, len, capacity) = {
             let last = &self.data.last().unwrap();
             let len = last.len();
@@ -23,28 +19,29 @@ impl Arena {
         };
         let offset = ((ptr + align - 1) & !(align - 1)) - ptr;
 
-        let result_ptr = if len + offset + bytes > capacity {
-            let mut exact: Vec<T> = Vec::with_capacity(1);
-            let (exact_ptr, exact_len, exact_cap) = (exact.as_mut_ptr(), exact.len(), exact.capacity());
-            mem::forget(exact);
-            let mut exact_bytes: Vec<u8> = unsafe { Vec::from_raw_parts(exact_ptr as *mut u8, exact_len * bytes, exact_cap * bytes) };
-            let result = exact_bytes.as_mut_ptr();
-            self.data.push(exact_bytes);
+        if len + offset + bytes > capacity {
+            let size = capacity.max(bytes);
+            self.data.push(Vec::with_capacity(size.saturating_add(size)));
+        }
 
-            self.next_size = self.next_size.saturating_add(self.next_size);
-            self.data.push(Vec::with_capacity(self.next_size));
+        let last = &mut self.data.last_mut().unwrap();
+        unsafe {
+            last.set_len(len + offset + bytes);
+            last.as_mut_ptr().offset((len + offset) as isize)
+        }
+    }
 
-            result
-        } else {
-            let last = &mut self.data.last_mut().unwrap();
-            unsafe {
-                last.set_len(len + offset + bytes);
-                last.as_mut_ptr().offset((len + offset) as isize)
-            }
-        };
-
-        let result: &'a mut T = unsafe { &mut *(result_ptr as *mut T) };
+    pub fn alloc<'a, T: Copy>(&mut self, value: T) -> &'a mut T where Self: 'a {
+        let ptr = self.alloc_bytes(mem::size_of::<T>(), mem::align_of::<T>());
+        let result: &'a mut T = unsafe { &mut *(ptr as *mut T) };
         *result = value;
+        result
+    }
+
+    pub fn alloc_slice<'a, T: Copy>(&mut self, values: &[T]) -> &'a mut [T] where Self: 'a {
+        let ptr = self.alloc_bytes(mem::size_of::<T>() * values.len(), mem::align_of::<T>());
+        let result: &'a mut [T] = unsafe { std::slice::from_raw_parts_mut(ptr as *mut T, values.len()) };
+        result.copy_from_slice(values);
         result
     }
 }
@@ -55,7 +52,7 @@ fn test_overflow() {
 
     let x: &mut u64 = arena.alloc(3);
     assert_eq!(*x, 3);
-    assert_eq!(arena.data.len(), 3);
+    assert_eq!(arena.data.len(), 2);
 }
 
 #[test]
@@ -73,4 +70,13 @@ fn test_alignment() {
 
     let x: &mut u64 = arena.alloc(3);
     assert_eq!(x as *mut u64 as usize % mem::align_of::<u64>(), 0);
+}
+
+#[test]
+fn test_slice() {
+    let mut arena = Arena::with_capacity(1024);
+
+    let xs: [u32; 16] = [0; 16];
+    let ys = arena.alloc_slice(&xs);
+    assert_eq!(xs, ys);
 }
